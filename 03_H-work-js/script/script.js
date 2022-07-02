599e7878458f10e825985d174ede61376a2a530d
NATALIE <natalyaiv1277@gmail.com>
2022-07-03 02:58:51 +0300

    home-work3

---
+++ 03_H-work-js/script/script.js
@@ -0,0 +1,79 @@
+// // hello
+
+// let firstVariable;
+
+// firstVariable = 'Hello World!';
+
+// console.log(firstVariable);
+
+// // let userName = 'Natalia';
+
+// // console.log(userName);
+
+// let wight;
+
+// wight = 77;
+
+// console.log('wight', wight);
+
+// let height;
+
+// height = wight;
+
+// console.log('height', height);
+
+// const number = 12; 
+
+// console.log('type of number', typeof number, number);
+
+// const string = 'Hello!';
+
+// console.log('type of string', typeof string, string);
+
+// // Home work-2
+
+// let userName = 'Diana';
+// let userSurname = 'Amazon';
+// let userAge = '30';
+// console.log(userName, userSurname, userAge, 'years old');
+
+// let newUser = 'KingArthur';
+// var onlineUser = 'Merlin';
+// console.log(newUser, onlineUser);
+
+// let listProducts = 'Dress, Shoes, Jeans, T-shirt, Sneakers'
+// console.log(listProducts);
+
+// let employeeSalary = 5000000;
+// let salary = '77000000 £ GBP';
+// console.log('your salary', employeeSalary, 'mine salary', salary);
+
+// let leftMouse = 55;
+// console.log('x=', leftMouse, 'px');
+
+// let topMouse = 77;
+// console.log('y=', topMouse, 'px');
+
+// let z_indexMouse = 2;
+// console.log('z-index =', z_indexMouse)
+
+// Home work-3 
+
+alert('Hello!');
+
+alert(5577);
+
+const message = 'Happy New life!';
+alert(message);
+
+const userMage = prompt('What is your name?');
+console.log('userMage', userMage)
+
+alert('Hello ' + userMage + '!');
+
+
+
+
+
+
+
